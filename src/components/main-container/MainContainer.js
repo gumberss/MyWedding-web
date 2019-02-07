d76d27167464c709259e97c2156e1fb7afae1859
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-02-07 00:02:06 -0200

    feat: add days left with fade present

--- src/components/main-container/MainContainer.js
+++ src/components/main-container/MainContainer.js
@@ -7,6 +7,36 @@ import flowers from '../../images/flowers.jpg'
 import { Keyframes, Frame } from 'react-keyframes'
 
 class MainContainer extends Component {
+
+    remainDays = (date1, date2) => {
+
+        var one_day = 1000 * 60 * 60 * 24;
+
+        var date1_ms = date1.getTime();
+        var date2_ms = date2.getTime();
+
+        // Calculate the difference in milliseconds
+        var difference_ms = date2_ms - date1_ms;
+
+        // Convert back to days and return
+        return Math.round(difference_ms / one_day);
+    }
+
+    daysLeftMessage = () => {
+
+        const daysLeft = this.remainDays(new Date(), new Date(2021, 0, 9))
+
+        if (daysLeft > 0)
+            return `𝐹𝒶𝓁𝓉𝒶𝓂 ${daysLeft} 𝒹𝒾𝒶𝓈`
+
+        if (daysLeft === 0)
+            return "É 𝒽𝑜𝒿𝑒!!"
+
+        const daysAfter = this.remainDays(new Date(2021, 0, 9), new Date())
+
+        return `𝒞𝒶𝓈𝒶𝒹𝑜𝓈 à ${daysAfter} 𝒹𝒾𝒶𝓈`
+    }
+
     render() {
         return (
             <div>
@@ -29,13 +59,33 @@ class MainContainer extends Component {
 
                 </Container>
 
-                <span style={styles.coupleNames} >𝓐𝓷𝓭𝓻𝓲𝓮𝓵𝓮 & 𝓖𝓾𝓼𝓽𝓪𝓿𝓸</span>
+                <Keyframes loop>
+                    <Frame duration={5000}>
+                        <span style={styles.coupleNames} >𝒜𝓃𝒹𝓇𝒾𝑒𝓁𝑒 & 𝒢𝓊𝓈𝓉𝒶𝓋𝑜</span>
+                    </Frame>
+
+                    <Frame duration={1000}>
+                        <span style={{...styles.coupleNames, ...styles.coupleNamesFadeOut}} >𝒜𝓃𝒹𝓇𝒾𝑒𝓁𝑒 & 𝒢𝓊𝓈𝓉𝒶𝓋𝑜</span>
+                    </Frame>
+
+                    <Frame duration={5000}>
+                        <span style={styles.coupleNames}>{this.daysLeftMessage()}</span>
+                    </Frame>
+                    <Frame duration={1000}>
+                        <span style={{...styles.coupleNames, ...styles.coupleNamesFadeOut}} >{this.daysLeftMessage()}</span>
+                    </Frame>
+                </Keyframes>
             </div>
         );
     }
 }
 
 const styles = {
+
+    coupleNamesFadeOut: {
+        opacity: '0'
+    },
+
     coupleNames: {
         position: 'absolute',
         marginRight: '0px',
@@ -45,7 +95,8 @@ const styles = {
         top: '20vh',
         fontSize: '8vh',
         color: 'white',
-        zIndex: 1000
+        zIndex: 1000,
+        transition: '1s',
     },
     startFlowers: {
         transition: '0.1s'
@@ -68,7 +119,7 @@ const styles = {
         overflow: 'hidden'
     },
     fade: {
-        position:'absolute',
+        position: 'absolute',
         zIndex: '100',
         backgroundColor: 'purple',
         opacity: '0.2',
